e6fe36b0d65e0a9560e010c988305000b3d81e24
sudipak177 <sudiprc177@gmail.com>
2024-09-09 11:37:53 +0545

    ddd

--- job_portal/routes/home.js
+++
@@ -1,8 +0,0 @@
-
-const express = require('express')
-const homeRouter = express.Router();
-const homeController = require('../controller/home_controller')
-
-homeRouter.get('/', homeController.renderHome);
-
-module.exports = homeRouter;
